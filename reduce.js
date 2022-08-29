a85a6267365f128cf2612d781ccb62e48f3f1ee7
Dennis_Bonesso <dennisbonesso@gmail.com>
2022-08-29 09:21:05 -0300

    reduce

---
+++ reduce.js
@@ -0,0 +1,22 @@
+const lista=[{
+  name:'sabao em po',
+  preco:30,
+},
+{
+  name:'cereal',
+  preco:12,
+},
+{
+  name:'toalha',
+  preco:30
+}]
+
+const saldoDisponivel=100
+
+
+function calculaSaldo(saldoDisponivel,lista){
+  return lista.reduce(function(prev,current){
+    return prev-current.preco
+  },saldoDisponivel)
+}
+
